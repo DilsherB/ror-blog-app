33aceee837ca58a391f3bb474188b7da9208865c
Dilsher Balouch <dilsherb@gmail.com>
2023-08-17 17:41:28 +0300

    models created

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
+
+  after_create :update_comments_counter
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  
+  after_create :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter, amount = 1)
+  end
+
+  def recent_comment(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+end
