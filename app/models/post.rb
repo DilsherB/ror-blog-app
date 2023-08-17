68e8c19810b5c9589c5cc63557f52ac69636ba9d
Dilsher Balouch <dilsherb@gmail.com>
2023-08-17 18:15:01 +0300

    linter errors removed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  
+
   after_create :update_posts_counter
 
   def update_posts_counter
-    author.increment!(:posts_counter, amount = 1)
+    author.increment!(:posts_counter, 1)
   end
 
   def recent_comment(limit = 5)
