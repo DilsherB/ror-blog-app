8530268a9e21a19b693d5fcc23e8909f402f6b80
Dilsher Balouch <dilsherb@gmail.com>
2023-08-18 07:56:47 +0300

    recent_post method test written

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -21,4 +21,12 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid 
   end
 
+  it "must return recent posts as per given arguments" do
+    user = User.create(name: "John", photo: "https://example.com", bio: "tttt", posts_counter:1)
+    post1 = Post.create(author: user, title: "title1", text: "text1", comments_counter: 1, likes_counter: 1)
+    post2 = Post.create(author: user, title: "title2", text: "text2", comments_counter: 1, likes_counter: 1)
+    post3 = Post.create(author: user, title: "title3", text: "text3", comments_counter: 1, likes_counter: 1)
+    expect(user.recent_posts(3)).to eq([post3, post2, post1])
+    
+  end
 end
