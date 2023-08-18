5200da58b8c48aa111029f4344385f7d320bf1cf
Dilsher Balouch <dilsherb@gmail.com>
2023-08-18 08:03:47 +0300

    title's validation tests written for post.rb

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject {Post.new()}
+  first_user = User.create(name: 'John', photo: 'https://example.com', bio: 'tttt', posts_counter: 1)
+  post1 = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+
+  it "Check title is not null" do
+    post1.title = nil
+    expect(post1).to_not be_valid
+  end
+
+  it "Check title is not greater than 250 characters" do
+    post1.title = "a" * 251
+    expect(post1).to_not be_valid
+  end
 end
