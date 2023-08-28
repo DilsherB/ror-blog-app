fb1e874367d58478bd4e1f05db2ba5131ea3d52c
Dilsher Balouch <dilsherb@gmail.com>
2023-08-28 13:13:24 +0300

    unlike made working

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :post, class_name: 'Post'
   belongs_to :author, class_name: 'User'
+  belongs_to :post
 
   after_create :update_comments_counter
+  after_destroy :update_comments_counter
 
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
