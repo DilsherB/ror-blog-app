40e98011e9096e8aeb132e8e834ab816a6cfe064
Dilsher Balouch <dilsherb@gmail.com>
2023-09-01 21:55:10 +0300

    api endpoints added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,30 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
-  # get "users", to: "users#index"
-  # get "users/:id", to: "users#show"
 
-  resources :users, only: [:index, :show] do
+  as :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  root 'users#index'
+  resources :users, only: [:index, :show, :destroy] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create, :destroy]
       get 'likes', to: 'likes#like', on: :member
     end
   end
+
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+  
 end
