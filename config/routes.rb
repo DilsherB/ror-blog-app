fd0ea42ef315ec944edbb0df3f5f89146261fe58
Dilsher Balouch <dilsherb@gmail.com>
2023-09-02 08:36:44 +0300

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  as :user do
-    get 'login' => 'devise/sessions#new', as: :new_user_session
-    post 'login' => 'devise/sessions#create', as: :user_session
-    get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
-  end
-
   root 'users#index'
+  
   resources :users, only: [:index, :show, :destroy] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create, :destroy]
@@ -15,16 +9,4 @@ Rails.application.routes.draw do
       get 'likes', to: 'likes#like', on: :member
     end
   end
-
-  
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:index] do
-        resources :posts, only: [:index] do
-          resources :comments, only: [:index, :create]
-        end
-      end
-    end
-  end
-  
 end
