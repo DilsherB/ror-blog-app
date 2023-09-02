1dea437da0c47cdc57a97d3aa5c21d810ad9b940
Dilsher Balouch <dilsherb@gmail.com>
2023-09-02 08:54:51 +0300

    Update comments_controller.rb

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,17 +4,7 @@ class Api::V1::CommentsController < ActionController::API
     render json: post.comments
   end
 
-  def create
-    post = Post.find(params[:post_id])
-    comment = post.comments.new(comment_params)
-    comment.author_id = current_user.id
-
-    if comment.save
-      render json: { notice: 'Comment successfully created', comment: }, status: :created
-    else
-      render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+ 
 
   private
 
