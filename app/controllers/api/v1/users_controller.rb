48993ec4ed41c4a0abab7b96844f72e133f55ed1
Dilsher Balouch <dilsherb@gmail.com>
2023-09-04 19:36:39 +0300

    linter errors corrected.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    users = User.includes(:posts => :comments).all
+    users = User.includes(posts: :comments).all
     json_data = users.to_json(
-      :include => {
-        :posts => {
-          :include => :comments
+      include: {
+        posts: {
+          include: :comments
         }
-      },
+      }
       # :except => [:created_at, :updated_at]
     )
     pretty_json = JSON.pretty_generate(JSON.parse(json_data))
